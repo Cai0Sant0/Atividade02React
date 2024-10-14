616ad3ebb7f2160cf5e4a1f9312a08d16f059dca
Caio Barbosa <caiomontebarbosa104@gmail.com>
2024-10-13 22:05:59 -0300

    Atividade02

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Texto from './components/Texto'
+
+function App() {
+
+  return (
+    <>
+    <Texto nome="Melissa"/>
+    <Texto nome="Caio"/>
+    <Texto nome="Obama"/>
+   </>
+  )
+}
+
+export default App
+
+
+// DIV
+// <> </>
+// Main Header Nav 
